d4e850e3f468faf27fb594a8c269ce3057973f95
Rami Chasygov <liketurbo.work@gmail.com>
2026-04-13 00:36:31 +0000

    Add transition for legend pills

--- src/components/Legend.jsx
+++ src/components/Legend.jsx
@@ -3,7 +3,7 @@ import styles from "./Legend.module.css";
 
 export default function Legend({ warehouse }) {
   return (
-    <div className={styles.legend}>
+    <div key={warehouse.id} className={styles.legend}>
       {warehouse.groups.map((g) => (
         <span key={g.id} className={styles.legend__pill} data-group={g.id}>
           <ShiftLabel shift={g} />
